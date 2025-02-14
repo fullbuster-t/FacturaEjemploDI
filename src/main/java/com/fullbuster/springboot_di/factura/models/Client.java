adeeca220a688f3144507ecedb6f2b11931195a2
Fullbuster <quinthony35@gmail.com>
2025-02-14 16:14:10 -0600

    Feature: Add value injection with @Value anotation

--- src/main/java/com/fullbuster/springboot_di/factura/models/Client.java
+++ src/main/java/com/fullbuster/springboot_di/factura/models/Client.java
@@ -1,11 +1,14 @@
 package com.fullbuster.springboot_di.factura.models;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Client {
 
+    @Value("${client.name}")
     private String name;
+    @Value("${client.lastname}")
     private String lastname;
 
 
