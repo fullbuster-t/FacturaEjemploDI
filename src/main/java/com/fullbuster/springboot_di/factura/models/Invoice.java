7a62bf04de88dec470644c1c4fd1400c9adb3f6c
Fullbuster <quinthony35@gmail.com>
2025-02-15 10:49:52 -0600

    Feature: implement getTotal Invoice

--- src/main/java/com/fullbuster/springboot_di/factura/models/Invoice.java
+++ src/main/java/com/fullbuster/springboot_di/factura/models/Invoice.java
@@ -43,6 +43,14 @@ public class Invoice {
     }
 
     public int getTotal() {
-        return 0;
+//        int total = 0;
+//        for (Item item : items) {
+//            total += item.getAmount();
+//        }
+
+        int total = items.stream()
+                .map(item -> item.getAmount())
+                .reduce(0, (sum, amount) -> sum + amount);
+        return total;
     }
 }
